7649ce778508d319eaff20a0d70ba29fbd31e92d
Jasbac0002 <125256049+Jasbac0002@users.noreply.github.com>
2023-02-24 12:33:48 +1300

    Update Test2.java
    remove 111

--- Test2.java
+++ Test2.java
@@ -2,7 +2,7 @@ public class test
 
 {
 	public static void main(String[] args){
-		System.out.println("Hello,World JASSSSPERRRRRR11111111111111111!");
+		System.out.println("Hello,World JASSSSPERRRRRR!");
 	}
 
 
